a94bcccf0cbe17901dfcba178485c2170d18d400
jordy-121 <jordykwando@gmail.com>
2026-04-08 08:58:54 +0700

    third commit

--- app/src/main/java/com/example/studentproject/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/studentproject/viewmodel/DetailViewModel.kt
@@ -1,8 +1,8 @@
 package com.example.studentproject.viewmodel
 
+import android.R
 import android.app.Application
-import android.app.DownloadManager
-import android.nfc.Tag
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -11,6 +11,8 @@ import com.android.volley.RequestQueue
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.studentproject.model.Student
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class DetailViewModel(application: Application): AndroidViewModel(application) {
     val studentLD = MutableLiveData<Student>()
@@ -18,11 +20,17 @@ class DetailViewModel(application: Application): AndroidViewModel(application) {
     val errorLD = MutableLiveData<Boolean>()
     var queue: RequestQueue?=null
 
-    fun fetch(student: Student) {
+    fun fetch(id: String) {
         queue = Volley.newRequestQueue(getApplication())
         val url = "https://www.jsonkeeper.com/b/LLmW"
         errorLD.value=false
-        val stringRequest = StringRequest(Request.Method.GET, url, {},{Log.d("volley_status",
+        val stringRequest = StringRequest(Request.Method.GET, url, {
+            val sType = object: TypeToken<List<Student>>(){}.type
+            val result = Gson().fromJson<List<Student>>(it, sType) as ArrayList
+            val student = result.find { it.id == id } as Student
+            studentLD.value = student
+        },{
+            Log.d("volley_status",
         it.message.toString())
 
         errorLD.value=true
@@ -33,6 +41,6 @@ class DetailViewModel(application: Application): AndroidViewModel(application) {
 //            "16055", "Nonie", "1998/03/28", "5718444778",
 //            "http://dummyimage.com/75x100.jpg/cc0000/ffffff"
 //        )
-        studentLD.value = student
+//        studentLD.value = studentLD
     }
 }
